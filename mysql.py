ed54d888fcc693f746c297fd283de8e4e69ff27e
tigercoin-world <shsfse@naver.com>
2022-08-28 22:56:01 +0900

    success connect mysql

--- mysql.py
+++ mysql.py
@@ -10,24 +10,14 @@ conn = pymysql.connect(host=os.environ.get("host"),
                        db=os.environ.get("database"), 
                        )
 cur = conn.cursor()
-# <user table>
-# chat_id int NOT NULL // 채팅 아이디
-# user_name char(20)// tele 아이디
-# first_name char(20)// tele 닉네임
-# languge_code char(10) // 언어
-# count_request int // 요청횟수
-# stake_key char(50)// wallet 주소
 
+# INSERT IGNORE INTO 'table name' (column1, column2)
+# VALUES (11, 13)
 
 # cur.execute()
 
 # stake key 입력
 stake_key = "insert into user (chat_id, user_name, first_name, languge_code, stake_key) values(%s, %s, %s, %s, %s)"
-# cur.execute(stake_key, (3, 'dori', 'khosla', 'ko', 1, 'slakfjs'))
-#start_value = (chat_id, user_name, first_name, lan_code, count_request, stake_key)
-# cur.execute(start)
-
-
 
 # get balance
 
