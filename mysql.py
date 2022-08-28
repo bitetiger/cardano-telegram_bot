71061969b02b179ef9e4c38e2f52ab46323dacf2
tigercoin-world <shsfse@naver.com>
2022-08-28 21:54:24 +0900

    modify mysql.py

--- mysql.py
+++ mysql.py
@@ -3,14 +3,12 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-host = "localhost"
-port = 3306
-database = "user"
-username = "user"
-password = 6589
-
-conn = pymysql.connect(host='localhost', user='root',
-                       password='6589', db='sys')
+conn = pymysql.connect(host=os.environ.get("host"), 
+                       user=os.environ.get("user"),
+                       password=os.environ.get("password"),
+                       port=os.environ.get("port"),
+                       db=os.environ.get("database"), 
+                       )
 cur = conn.cursor()
 # <user table>
 # chat_id int NOT NULL // 채팅 아이디
@@ -26,7 +24,7 @@ cur = conn.cursor()
 # /start stake key 입력
 start = "insert into user values(%d, %s, %s, %s, %d, %s)"
 #start_value = (chat_id, user_name, first_name, lan_code, count_request, stake_key)
-start_values = (12412, 'user_name', 'first_name', 'ko', 1, 'stake_key')
+start_values = (1, 'user_name', 'first_name', 'ko', 1, 'stake_key')
 
 cur.execute(start, start_values)
 
